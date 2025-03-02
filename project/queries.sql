099c5f9495b500018ae0bf0363aa35b97ce9e1eb
angelalvcam <fluttershady@gmail.com>
2025-03-02 16:01:24 -0600

    modified and added queries

--- project/queries.sql
+++ project/queries.sql
@@ -27,6 +27,17 @@ JOIN "releases" ON "releases"."album_id" = "albums"."id"
 JOIN "artists" ON "releases"."artist_id" = "artists"."id"
 WHERE "artists"."name" = 'Linkin Park'; 
 
+-- Get all the songs in 'The Matrix Reloaded Sountrack' album with its details
+SELECT "includes"."track", "songs"."name", "songs"."length", "songs"."genre", group_concat("artists"."name", ', ') AS "artists"
+FROM "songs"
+JOIN "includes" ON "includes"."song_id" = "songs"."id"
+JOIN "albums" ON "includes"."album_id" = "albums"."id"
+JOIN "contributes" ON "contributes"."song_id" = "songs"."id"
+JOIN "artists" ON "contributes"."artist_id" = "artists"."id"
+WHERE "albums"."name" = 'The Matrix Reloaded Soundtrack'
+GROUP BY "songs"."id"
+ORDER BY "includes"."track";
+
 -- Get all the songs in '12:00' album with its details
 SELECT "includes"."track", "songs"."name", "songs"."length", "songs"."genre", group_concat("artists"."name", ', ') AS "artists"
 FROM "songs"
@@ -39,7 +50,7 @@ GROUP BY "songs"."id"
 ORDER BY "includes"."track";
 
 -- Get all the songs in 'K-Pop Vibes' playlist
-SELECT "songs"."name", "length"
+SELECT "playlist_order" AS "order", "songs"."name", "length"
 FROM "playlists"
 JOIN "contains" ON "playlists"."id" = "contains"."playlist_id"
 JOIN "songs" ON "contains"."song_id" = "songs"."id"
