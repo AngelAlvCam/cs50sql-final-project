8718422c6cec234df4cb60bd6d7e65b73794f9be
angelalvcam <fluttershady@gmail.com>
2025-03-02 15:03:06 -0600

    modified load_data script

--- project/load_data.sql
+++ project/load_data.sql
@@ -1,11 +1,12 @@
 .import --csv --skip 1 data/albums.csv albums
 .import --csv --skip 1 data/artists.csv artists
+.import --csv --skip 1 data/playlists.csv playlists
+.import --csv --skip 1 data/songs.csv songs
+.import --csv --skip 1 data/users.csv users
 .import --csv --skip 1 data/contains.csv contains
 .import --csv --skip 1 data/contributes.csv contributes
 .import --csv --skip 1 data/follows.csv follows
 .import --csv --skip 1 data/includes.csv includes
 .import --csv --skip 1 data/likes.csv likes
-.import --csv --skip 1 data/playlists.csv playlists
 .import --csv --skip 1 data/releases.csv releases
-.import --csv --skip 1 data/songs.csv songs
-.import --csv --skip 1 data/users.csv users
+UPDATE "releases" SET "artist_id" = NULL WHERE "artist_id" = '';
